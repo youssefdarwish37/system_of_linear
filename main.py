2ed0a13bfd57f83e8784be423295cbc88cae9342
youssefdarwish37 <85325600+youssefdarwish37@users.noreply.github.com>
2022-01-01 05:40:04 +0200

    gauss finished

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def LU(A):
         #  Make the rows bellow this one zero in the current column
         for k in range(i + 1, n):
             U[k][i] = 0
+    print(U)
     n = len(L)
 
     # (5) Perform substitutioan Ly=b
@@ -88,7 +89,65 @@ def LU(A):
     return x
 
 
+def gauss_elimination(A):
+    n = len(A) # Give us total of lines
+    # Find both U and L matrices
+    for i in range(0, n):  # for i in [0,1,2,..,n]
+        # Find the maximun value in a column in order to change lines
+        maxElem = abs(A[i][i])
+        maxRow = i
+        for k in range(i + 1, n):  # Interacting over the next line
+            if abs(A[k][i]) > maxElem:
+                maxElem = abs(A[k][i])  # Next line on the diagonal
+                maxRow = k
+
+        # Swap the rows pivoting the maxRow, i is the current row
+        for k in range(i, n+1):  # Interacting column by column
+            tmp = A[maxRow][k]
+            A[maxRow][k] = A[i][k]
+            A[i][k] = tmp
+
+        #  Subtract lines
+        for k in range(i + 1, n):
+            c = -A[k][i] / float(A[i][i])
+            for j in range(i, n+1):
+                A[k][j] += c * A[i][j]  # Multiply with the pivot line and subtract
+
+        #  Make the rows bellow this one zero in the current column
+        for k in range(i + 1, n):
+            A[k][i] = 0
+    # A=[X|b]
+    # b vector
+    b = [0 for i in range(n)]
+    for i in range(0, n):
+        b[i] = A[i][n]
+
+    # Fill X matrix
+    X = [[0 for i in range(0, n)] for i in range(n)]
+    for i in range(0, n):
+        for j in range(0, n):
+            X[i][j] = A[i][j]
+    # backward substitution
+    x = [0 for i in range(n)]
+    for i in range(len(X) - 1, -1, -1):
+        x[i] = b[i]
+        for j in range(0, len(X[0])):
+            if i != j:
+                x[i] -= x[j] * X[i][j]
+        x[i] = x[i] / X[i][i]
+
+    print('X:')
+    for row in X:
+        print(row)
+
+    for i in range(len(x)):
+        print(f'd{i + 1}={x[i]}')
+
+    return x
+
+
 if __name__ == '__main__':
     m = [[8, 4, -1, 11], [-2, 3, 1, 4], [2, -1, 6, 7]]
-    x = LU(m)
-
+    m2 = [[1, 1, -1, -3], [6, 2, 2, 2], [-3, 4, 1, 1]]
+   # x = LU(m)
+    gauss_elimination(m2)
