3928fc5100d888e1f457991cd6a1240941ab5570
youssefdarwish37 <85325600+youssefdarwish37@users.noreply.github.com>
2022-01-05 21:55:37 +0200

    gauss jordan finished

--- main.py
+++ main.py
@@ -27,13 +27,11 @@ def LU(A):
     # Find both U and L matrices
     for i in range(0, n):
         # Find the maximun value in a column in order to change lines
-        maxElem = abs(U[i][i])
         maxRow = i
-        for k in range(i + 1, n):
-            if abs(U[k][i]) > maxElem:
-                maxElem = abs(U[k][i])
-                maxRow = k
 
+        for k in range(i + 1, n):
+            if abs(U[k][i]) > abs(U[maxRow][i]):
+                maxRow = k
         # Swap the rows pivoting the maxRow
         for k in range(i, n):
             tmp = U[maxRow][k]
@@ -88,12 +86,11 @@ def gauss_elimination(A):
     n = len(A)  # Give us total of lines
 
     for i in range(0, n):
-        # Find the maximun value in a column in order to change lines
-        maxElem = abs(A[i][i])
+        # Find the maximum value in a column in order to change lines
         maxRow = i
+
         for k in range(i + 1, n):
-            if abs(A[k][i]) > maxElem:
-                maxElem = abs(A[k][i])  # Next line on the diagonal
+            if abs(A[k][i]) > abs(A[maxRow][i]):
                 maxRow = k
 
         # Swap the rows pivoting the maxRow
@@ -138,6 +135,74 @@ def gauss_elimination(A):
     return x
 
 
+def gauss_jordan(A):
+    n = len(A)  # Give us total of lines
+
+    for i in range(0, n):
+        #  Find the maximum value in a column in order to change lines
+        maxRow = i
+        #
+        for k in range(i + 1, n):
+            if abs(A[k][i]) > abs(A[maxRow][i]):
+                maxRow = k
+
+        # Swap the rows pivoting the maxRow
+        for k in range(i, n + 1):
+            tmp = A[maxRow][k]
+            A[maxRow][k] = A[i][k]
+            A[i][k] = tmp
+
+        # making main diagonal =1
+        m = float(A[i][i])
+        for k in range(i, n + 1):
+            A[i][k] = A[i][k] / m
+        #  Subtract lines
+        for k in range(i + 1, n):
+            c = A[k][i] / float(A[i][i])
+            for j in range(i, n + 1):
+                A[k][j] -= c * A[i][j]  # Multiply with the pivot line and subtract
+
+        if i > 0:
+            for k in range(0, i):
+                d = A[k][i] / float(A[i][i])
+
+                for j in range(k, n+1):
+                    A[k][j] -= d * A[i][j]  # Multiply with the pivot line and subtract
+
+        #  Subtract lines
+        for k in range(i + 1, n):
+            c = A[k][i] / float(A[i][i])
+            for j in range(i, n + 1):
+                A[k][j] -= c * A[i][j]  # Multiply with the pivot line and subtract
+        # A=[X|b]
+        # b vector
+    b = [0 for i in range(n)]
+    for i in range(0, n):
+        b[i] = A[i][n]
+
+    # Fill X matrix
+    X = [[0 for i in range(0, n)] for i in range(n)]
+    for i in range(0, n):
+        for j in range(0, n):
+            X[i][j] = A[i][j]
+        # backward substitution
+    x = [0 for i in range(n)]
+    for i in range(len(X) - 1, -1, -1):
+        x[i] = b[i]
+        for j in range(0, len(X[0])):
+            if i != j:
+                x[i] -= x[j] * X[i][j]
+        x[i] = x[i] / X[i][i]
+    print('X:')
+    for row in X:
+        print(row)
+
+    for i in range(len(x)):
+        print(f'd{i + 1}={x[i]}')
+
+    return x
+
+
 def gauss_seidel(a, epsilon):
     x0, y0, z0 = 0, 0, 0
     # iterations += '%d \t [%.6f \t %.6f\t %.6f]\n' % (0, x1, x2, x3)
@@ -160,6 +225,8 @@ if __name__ == '__main__':
     m = [[8, 4, -1, 11], [-2, 3, 1, 4], [2, -1, 6, 7]]
     m2 = [[1, 1, -1, -3], [6, 2, 2, 2], [-3, 4, 1, 1]]
     m3 = [[12, 3, -5, 1], [1, 5, 3, 28], [3, 7, 13, 76]]
-    x = LU(m)
+    m4 = [[2, 3, 1, -4], [4, 1, 4, 9], [3, 4, 6, 0]]
+    x = LU(m2)
     y = gauss_elimination(m2)
-    gauss_seidel(m3, 0.7)
+    z = gauss_jordan(m2)
+    gauss_seidel(m2, 0.7)
